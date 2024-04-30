3fb195b8d7d98065be984f5f691c192572eec350
Diego Santos <63413446+Diegodevops26@users.noreply.github.com>
2024-04-30 20:58:10 -0300

    Update SocialNetworkContainer.jsx

--- src/components/SocialNetworkContainer.jsx
+++ src/components/SocialNetworkContainer.jsx
@@ -3,16 +3,16 @@ import { FaLinkedinIn, FaGithub, FaInstagram } from "react-icons/fa";
 import "../styles/components/socialnetworkcontainer.sass";
 
 const socialNetworks = [
-  { name: "linkedin", icon: <FaLinkedinIn /> },
-  { name: "github", icon: <FaGithub /> },
-  { name: "instagram", icon: <FaInstagram /> },
+  { name: "linkedin", icon: <FaLinkedinIn />, url: "https://www.linkedin.com/in/diego-santos-83b109157/" },
+  { name: "github", icon: <FaGithub />, url: "https://github.com/Diegodevops26" },
+  { name: "instagram", icon: <FaInstagram />, url: "https://www.instagram.com/diego_santos.dev/?next=%2F" },
 ];
 
 const SocialNetworkContainer = () => {
   return (
     <section id="social-networks">
-      {socialNetworks.map((network) => (
-        <a href="https://github.com/Diegodevops26" className="social-btn" id={network.name} key={network.name}>
+     {socialNetworks.map((network) => (
+        <a href={network.url} className="social-btn" id={network.name} key={network.name}>
           {network.icon}
         </a>
       ))}
