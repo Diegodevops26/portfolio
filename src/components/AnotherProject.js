d9e434c04990b49b842da1cdab8ae2e84636b2ac
Diego Santos <63413446+Diegodevops26@users.noreply.github.com>
2024-12-27 18:33:02 -0300

    Create AnotherProject.js

---
+++ src/components/AnotherProject.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import countriesImage from '../img/countries.jfif'; // Imagem do projeto
+
+const AnotherProject = () => {
+  return (
+    <div className="another-project">
+      <h3>Lista de Países com Next.js</h3>
+      <img src={countriesImage} alt="Countries Project Screenshot" className="project-image" />
+      <p>
+        Este projeto foi desenvolvido utilizando Next.js 13 e a API REST Countries. 
+        Os principais recursos incluem:
+        <ul>
+          <li>Exibição de uma lista de países com suas bandeiras e nomes em português.</li>
+          <li>Detalhes do país em uma nova rota, incluindo capital, população e línguas faladas.</li>
+          <li>Funcionalidade de busca e exibição de países que fazem fronteira.</li>
+        </ul>
+      </p>
+      <a href="https://github.com/SeuUsuario/ListaDePaises" className="btn">
+        Ver Projeto no GitHub
+      </a>
+    </div>
+  );
+};
+
+export default AnotherProject;
